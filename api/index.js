15e26553bb597e7a8f5909341e2ff5d9a039a93e
Your Name <ptsvyatkov97@gmail.com>
2023-02-07 20:59:20 +0100

    deployfix2

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(path.join(__dirname + '/uploads')))
 app.use(cors({
     credentials: true,
-    origin: 'https://tourjourney.vercel.app/'
+    origin: 'https://tourjourney.vercel.app'
 }));
 
 mongoose.connect(process.env.MONGO_URL);
