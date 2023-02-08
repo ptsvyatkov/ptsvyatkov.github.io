6b5528c8a4554a90306ef4930402ebf082571f3f
Your Name <ptsvyatkov97@gmail.com>
2023-02-08 01:56:50 +0100

    updateURL

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,13 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import "/node_modules/flag-icons/css/flag-icons.min.css";
 import App from './App';
 import './index.css'
-import { BrowserRouter, Navigate } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
 
+axios.defaults.baseURL = 'https://apitourjourney.onrender.com';
+axios.defaults.withCredentials = true;
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .use(LanguageDetector)
